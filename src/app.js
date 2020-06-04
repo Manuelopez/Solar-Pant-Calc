57979bcd5d6524311b4dfc4dd8dc6943afbea804
Manuel <manuelopez18@hotmail.com>
2020-06-03 21:28:54 -0400

    heroku ready

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const getSolarData = require('./hottel');
 const path = require('path');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 
